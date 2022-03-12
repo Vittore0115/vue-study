1de51ce2de71f5818cf82410bd8a61e680cbfe1d
zwt <1607458660@qq.com>
2022-03-12 10:24:21 +0800

    💥 feat: 完善text、html指令 update更新

--- mini-vue/myVue.js
+++ mini-vue/myVue.js
@@ -52,8 +52,16 @@ class Compile {
       // 2、判断node类型
       if (node.nodeType === 1) {
         // 元素
-        console.log('element', node.nodeName)
+        // console.log('element', node.nodeName)
         this.createElement(node)
+        // 递归
+        if (node.childNodes.length > 0) {
+          this.compile(node);
+        }
+      }else if (this.isInter(node)) {
+        // 插值文本
+        // console.log('text', node.textContent);
+        this.compileText(node);
       }
     })
   }
@@ -71,18 +79,50 @@ class Compile {
         const dir = attrName.substring(3)
         // 判断是否存在指令处理函数，若存在则调用它
         this[dir] && this[dir](node, exp)
-      }
+      } 
     })
   }
 
   // 处理my-html
   html (node, exp) {
-    node.innerHTML = this.$vm[exp]
+    // node.innerHTML = this.$vm[exp]
+    this.update(node, exp, "html");
   }
 
   // 处理my-text
   text (node, exp) {
-    node.textContent = this.$vm[exp]
+    // node.textContent = this.$vm[exp]
+    this.update(node, exp, "text");
+  }
+
+  // {{ooxx}}
+  isInter(node) {
+    return node.nodeType === 3 && /\{\{(.*)\}\}/.test(node.textContent);
+  }
+
+  // 处理插值文本 {{xx}}
+  compileText(node) {
+    this.update(node, RegExp.$1, "text");
+  }
+
+  // 统一做初始化和更新处理
+  update(node, exp, dir) {
+    // 初始化
+    const fn = this[dir + "Updater"];
+    fn && fn(node, this.$vm[exp]);
+
+    // 更新
+    new Watcher(this.$vm, exp, function(val) {
+      fn && fn(node, val);
+    })
+  }
+
+  textUpdater(node, val) {
+    node.textContent = val;
+  }
+
+  htmlUpdater(node, val) {
+    node.innerHTML = val;
   }
 }
 
@@ -90,9 +130,14 @@ function defineReactive (obj, key, val) {
   // 如果val本身还是对象，则需要递归处理
   obserable(val)
 
+  // 创建一个Dep实例和key对应
+  const dep = new Dep()
+
   Object.defineProperty(obj, key, {
     get () {
       console.log('get:', key)
+      // 依赖收集
+      Dep.target && dep.addDep(Dep.target)
       return val
     },
     set (v) {
@@ -101,6 +146,42 @@ function defineReactive (obj, key, val) {
       }
       obserable(v)
       console.log('set:', key)
+      // update()
+      dep.notify()
     }
   })
+}
+
+// 负责具体更新任务的Watcher
+class Watcher {
+  constructor(vm, key, updateFn) {
+    this.vm = vm;
+    this.key = key;
+    this.updateFn = updateFn;
+
+    // 触发依赖收集
+    Dep.target = this
+    vm[key]
+    Dep.target = null
+  }
+
+  update() {
+    this.updateFn.call(this.vm, this.vm[this.key]);
+  }
+}
+
+// 和data中响应式key之间是一一对应关系
+class Dep {
+  constructor() {
+    // 保存关联的watcher实例
+    this.deps = []
+  }
+
+  addDep(dep) {
+    this.deps.push(dep)
+  }
+
+  notify() {
+    this.deps.forEach(dep => dep.update())
+  }
 }
